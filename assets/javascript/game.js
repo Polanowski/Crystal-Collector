b4543163421697ead617d6f82033a623746902ec
Polanowski <Polanowski@users.noreply.github.com>
2017-10-14 20:04:23 -0600

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,27 +10,17 @@ Will need four crystals and they will also have random generated numbers.
 Need to have the total score compare to the computer's guess and evaluate when the game is won or lost.
 After each win/loss need a counter
 =============================================*/
-$(document).ready(function(){
-var wins = 0;
+//Declared variables for storing the wins, losses
+var wins = 0; 
 var losses = 0;
-var text_value = "";
-// function intialize(comp){
-// 	comp = Math.floor(Math.random() * 102) + 19;
-// 	console.log(comp);
-// }
-
-// intialize(compStore);
-
-// function crystal(){
-// 	diamond = Math.floor(Math.random() * 6) + 3;
-// 	console.log(diamond);
-// }
+//the total score of the clicked crystals
+var score = 0;
 
 //Stores the computer number in compStore
-var compStore = Math.floor(Math.random() * 102) + 19;
+var compStore = Math.floor(Math.random() * 102) + 19; //
 //Displays the random number
 
-$("#com-number").html(compStore);
+
 
 
 var ruby = Math.floor(Math.random() * 5) + 3;
@@ -38,30 +28,66 @@ var diamond = Math.floor(Math.random() * 4) + 3;
 var sapphire = Math.floor(Math.random() * 6) + 3;
 var emerald = Math.floor(Math.random() * 7) + 3;
 
+var gameSetUP = function(){
+	$(".win").empty();
+	$(".win").append(wins);
+	$(".loss").empty();
+	$(".loss").append(losses);
+	$("#value").empty();
+	$("#value").append(score);
+	
+}
+
+var reset = function(){
+	score = 0;
+    compStore = Math.floor((Math.random()* 100) + 19);
+
+	$("#com-number").empty();
+	$("#com-number").append(compStore);
+
+	ruby = Math.floor(Math.random() * 5) + 3;
+	diamond = Math.floor(Math.random() * 4) + 3;
+	sapphire = Math.floor(Math.random() * 6) + 3;
+	emerald = Math.floor(Math.random() * 7) + 3;
+    gameSetUP();
+}
+
+
 console.log(diamond, ruby, emerald, sapphire);//testing purposes
+var game = function (){
+	//evualting if the score is equal to the displayed number
+	if (score === compStore) {
+		wins = wins + 1;
+		reset();
+	}
+	else if(score > compStore){
+		losses = losses + 1;
+		reset();
+	}
+	else{
+		gameSetUP();
+	}
+}
+$(document).ready(function(){
+
+	$("#com-number").append(compStore);
 
 
-$(".diamond").on("click", function(crystal){
-	$(".your-total").html(diamond);
-	var text_value = $("#value").text();
-	console.log(this.text_value);
-});
-
-$(".ruby").on("click", function(){
-	$(".your-total").html(ruby);
-
-});
-
-$(".emerald").on("click", function(){
-	$(".your-total").html(emerald);
-
-});
-
-$(".sapphire").on("click", function(){
-	$(".your-total").html(sapphire);
-
-});
-
-
+	$(".diamond").click(function(){
+		score = score + diamond;
+		game();
+	});
+	$(".ruby").click(function(){
+		score  = score + ruby;
+		game();
+	});
+	$(".sapphire").click(function(){
+		score  = score + sapphire;
+		game();
+	});
+	$(".emerald").click(function(){
+		score  = score + emerald;
+		game();
+	});
 
 });
